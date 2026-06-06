e803a52be60e841fb9e01de1e7d9a2343344b169
Sergey Mosykin <smosyakin@yandex.ru>
2026-06-06 17:40:40 +0300

    fixing serializers.py, views.py (materials)

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,17 +1,39 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from materials.models import Course, Lesson
+from users.models import Payment
 
+
+class LessonSerializer(ModelSerializer):
+    courses = SerializerMethodField()
+
+    def get_courses(self, lesson):
+        return [course.course_name for course in Course.objects.filter(lesson=lesson)]
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"
 
 class CourseSerializer(ModelSerializer):
-
+    lesson = LessonSerializer()
     class Meta:
         model = Course
         fields = "__all__"
 
 
-class LessonSerializer(ModelSerializer):
+class CourseDetailSerializer(ModelSerializer):
+    quantity_lessons_in_course = SerializerMethodField()
+    lesson = LessonSerializer()
+
+    def get_quantity_lessons_in_course(self, course):
+        return Course.objects.filter(lesson=course.lesson).count()
 
     class Meta:
-        model = Lesson
-        fields = "__all__"
+        model = Course
+        fields = ("course_name", "quantity_lessons_in_course")
+
+
+class PaymentSerializer(ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
--- materials/views.py
+++ materials/views.py
@@ -1,15 +1,25 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
-from materials.serializers import CourseSerializer, LessonSerializer
+from materials.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer, PaymentSerializer
+from users.models import Payment
 
 
 class CourseViewSet(ModelViewSet):
-    serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ('lesson', 'course')
+
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return CourseDetailSerializer
+        return CourseSerializer
 
 
 class LessonCreateAPIView(CreateAPIView):
@@ -33,3 +43,12 @@ class LessonUpdateAPIView(UpdateAPIView):
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+
+
+class PaymentViewSet(ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method')
+    ordering_fields = ('payment_date',)
+    ordering = ['-payment_date']
