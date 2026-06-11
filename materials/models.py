9e18fdd5cd2edd4d0d1c4d055849d24fc0ff4125
Sergey Mosykin <smosyakin@yandex.ru>
2026-06-11 15:03:45 +0300

    fixing materials: serializers.py, models.py, views.py, urls.py

--- materials/models.py
+++ materials/models.py
@@ -8,13 +8,18 @@ class Course(models.Model):
     )
     description = models.TextField(null=True, blank=True, verbose_name="Описание курса")
 
-    # def __str__(self):
-    #     return self.course_name
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Владелец",
+        help_text="Укажите владельца курса",
+    )
 
     class Meta:
         verbose_name = "курс"
         verbose_name_plural = "курсы"
-        # ordering = ["course_name"]
 
 
 class Lesson(models.Model):
@@ -36,10 +41,72 @@ class Lesson(models.Model):
         blank=True,
     )
 
-    # def __str__(self):
-    #     return self.lesson_name
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Владелец",
+        help_text="Укажите владельца урока",
+    )
 
     class Meta:
         verbose_name = "урок"
         verbose_name_plural = "уроки"
-        # ordering = ["lesson_name", "course_name"]
+
+
+# from django.db import models
+#
+# from users.models import User
+#
+#
+# class Course(models.Model):
+#     course_name = models.CharField(max_length=200, verbose_name="Название курса")
+#     photo = models.ImageField(
+#         upload_to="course/photo", null=True, blank=True, verbose_name="Изображение"
+#     )
+#     description = models.TextField(null=True, blank=True, verbose_name="Описание курса")
+#
+#     owner = models.ForeignKey(User,
+#                               on_delete=models.SET_NULL,
+#                               null=True,
+#                               blank=True,
+#                               verbose_name="Владелец",
+#                               help_text="Укажите владельца курса"
+#                               )
+#
+#     # def __str__(self):
+#     #     return self.course_name
+#
+#     class Meta:
+#         verbose_name = "курс"
+#         verbose_name_plural = "курсы"
+#         # ordering = ["course_name"]
+#
+#
+# class Lesson(models.Model):
+#     lesson_name = models.CharField(max_length=150, verbose_name="Название урока")
+#     description = models.TextField(null=True, blank=True, verbose_name="Описание урока")
+#     photo = models.ImageField(
+#         upload_to="lesson/photo", null=True, blank=True, verbose_name="Изображение"
+#     )
+#     video_url = models.URLField(
+#         max_length=500, null=True, blank=True, verbose_name="Ссылка на видео"
+#     )
+#
+#     course_name = models.ForeignKey(
+#         Course,
+#         on_delete=models.SET_NULL,
+#         verbose_name="Название курса",
+#         related_name="lesson_name",
+#         null=True,
+#         blank=True,
+#     )
+#
+#     # def __str__(self):
+#     #     return self.lesson_name
+#
+#     class Meta:
+#         verbose_name = "урок"
+#         verbose_name_plural = "уроки"
+#         # ordering = ["lesson_name", "course_name"]
--- materials/views.py
+++ materials/views.py
@@ -1,13 +1,24 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework.generics import (CreateAPIView, DestroyAPIView,
-                                     ListAPIView, RetrieveAPIView,
-                                     UpdateAPIView)
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
-from materials.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer, PaymentSerializer
+from materials.serializers import (
+    CourseDetailSerializer,
+    CourseSerializer,
+    LessonSerializer,
+    PaymentSerializer,
+)
 from users.models import Payment
+from users.permissions import IsModer, IsOwner
 
 
 class CourseViewSet(ModelViewSet):
@@ -20,34 +31,59 @@ class CourseViewSet(ModelViewSet):
             return CourseDetailSerializer
         return CourseSerializer
 
+    def perform_create(self, serializer):
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = (~IsModer,)
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = (IsModer | IsOwner,)
+        elif self.action == "destroy":
+            self.permission_classes = (~IsModer | IsOwner,)
+        return super().get_permissions()
+
 
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = (~IsModer, IsAuthenticated)
 
 
 class LessonListAPIView(ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
+    def perform_create(self, serializer):
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
+
+
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
+
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner | ~IsModer)
 
 
 class PaymentViewSet(ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method')
-    ordering_fields = ('payment_date',)
-    ordering = ['-payment_date']
+    filterset_fields = ("paid_course", "paid_lesson", "payment_method")
+    ordering_fields = ("payment_date",)
+    ordering = ["-payment_date"]
