bf78804738d9d3ddbe1a1ac6fa9e6f283ae49387
Sergey Mosykin <smosyakin@yandex.ru>
2026-06-13 21:51:38 +0300

    add tests.py

--- materials/tests.py
+++ materials/tests.py
@@ -1,3 +1,149 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from materials.models import Course, Lesson
+from users.models import User
+
+
+class CourseTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="admin@sky.pro")
+        self.course = Course.objects.create(course_name="Python", description="Хороший курс}", owner=self.user)
+        self.lesson = Lesson.objects.create(lesson_name="DRF", course_name=self.course, owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_course_retrieve(self):
+        url = reverse(viewname="materials:course-detail", args=(self.course.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get("course_name"), self.course.course_name
+        )
+
+    def test_course_create(self):
+        url = reverse("materials:course-list")
+        data = {
+            "course_name": "Python"
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            Course.objects.all().count(), 2
+        )
+
+    def test_course_update(self):
+        url = reverse(viewname="materials:course-detail", args=(self.course.pk,))
+        data = {
+            "course_name": "Python"
+        }
+        response = self.client.patch(url, data)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get("course_name"), "Python"
+        )
+
+    def test_course_delete(self):
+        url = reverse(viewname="materials:course-detail", args=(self.course.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT
+        )
+        self.assertEqual(
+            Course.objects.all().count(), 0
+        )
+
+    def test_course_list(self):
+        url = reverse("materials:course-list")
+        response = self.client.get(url)
+        data = response.json()
+        result = {'count': 1, 'next': None, 'previous': None, 'results':
+            [{'id': 4, 'is_subscribed': False, 'course_name': 'Python', 'photo': None,
+              'description': 'Хороший курс}', 'owner': 3}]}
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data, result
+        )
+
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="admin@sky.pro")
+        self.course = Course.objects.create(course_name="Python", description="Хороший курс}", owner=self.user)
+        self.lesson = Lesson.objects.create(lesson_name="DRF", course_name=self.course, owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_lesson_retrieve(self):
+        url = reverse(viewname="materials:lesson_get", args=(self.lesson.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get("lesson_name"), self.lesson.lesson_name
+        )
+
+    def test_lesson_create(self):
+        url = reverse("materials:lesson_create")
+        data = {
+            "lesson_name": "Django"
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            Lesson.objects.all().count(), 2
+        )
+
+    def test_lesson_update(self):
+        url = reverse(viewname="materials:lesson_update", args=(self.lesson.pk,))
+        data = {
+            "lesson_name": "Postgres"
+        }
+        response = self.client.patch(url, data)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get("lesson_name"), "Postgres"
+        )
+
+    def test_lesson_delete(self):
+        url = reverse(viewname="materials:lesson_delete", args=(self.lesson.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT
+        )
+        self.assertEqual(
+            Lesson.objects.all().count(), 0
+        )
+
+    def test_lesson_list(self):
+        url = reverse("materials:lesson_list")
+        response = self.client.get(url)
+        data = response.json()
+        print(data)
+        result = {'count': 1, 'next': None, 'previous': None, 'results':
+            [{'id': 9, 'video_url': None, 'lesson_name': 'DRF', 'description': None,
+              'photo': None, 'course_name': 9, 'owner': 8}]}
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data, result
+        )
