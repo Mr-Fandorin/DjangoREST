e972c0ed12bedbff6f9bc70bfdb837702c7e4859
Sergey Mosykin <smosyakin@yandex.ru>
2026-07-23 11:06:33 +0300

    fixing urls.py, views.py (materials), urls.py (users)

--- materials/views.py
+++ materials/views.py
@@ -2,13 +2,9 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status
-from rest_framework.generics import (
-    CreateAPIView,
-    DestroyAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-    UpdateAPIView,
-)
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, RetrieveAPIView,
+                                     UpdateAPIView)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -16,12 +12,8 @@ from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, CourseSubscription, Lesson
 from materials.paginations import CustomPagination
-from materials.serializers import (
-    CourseDetailSerializer,
-    CourseSerializer,
-    LessonSerializer,
-    PaymentSerializer,
-)
+from materials.serializers import (CourseDetailSerializer, CourseSerializer,
+                                   LessonSerializer, PaymentSerializer)
 from materials.services import create_checkout_session
 from users.models import Payment
 from users.permissions import IsModer, IsOwner
